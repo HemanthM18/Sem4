c670b6e6c28f321e70a814878dcfb3b10f0198df
HemanthM18 <mhemanth3131@gmail.com>
2025-02-19 16:20:14 +0530

    Added pics and some changes in css

--- Simulated Work V3/Rock Paper Scissors/app.js
+++ Simulated Work V3/Rock Paper Scissors/app.js
@@ -1,43 +1,45 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
 
-possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
-}))
+possibleChoices.forEach(button => 
+  button.addEventListener('click', (e) => {
+    userChoice = e.target.closest('button').id; // In case the click is on the image
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+  })
+);
 
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * 3) + 1
-    if(randomNumber === 1){
-        computerChoice = 'rock'
-    }
-    if(randomNumber === 2){
-        computerChoice = 'paper'
-    }
-    if(randomNumber === 3){
-        computerChoice = 'scissors'
-    }
-    computerChoiceDisplay.innerHTML = computerChoice
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    computerChoice = 'rock';
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'paper';
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'scissors';
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
-function getResult(){
-    if (userChoice === computerChoice) {
-        result = 'Its a tie!'
-    } else if (
-        (userChoice === 'rock' && computerChoice === 'scissors') ||
-        (userChoice === 'scissors' && computerChoice === 'paper') ||
-        (userChoice === 'paper' && computerChoice === 'rock')
-    ) {
-        result = 'You win!'
-    } else {
-        result = 'You lose!'
-    }
-    resultDisplay.innerHTML = result
+function getResult() {
+  if (userChoice === computerChoice) {
+    result = 'It\'s a tie!';
+  } else if (
+    (userChoice === 'rock' && computerChoice === 'scissors') ||
+    (userChoice === 'scissors' && computerChoice === 'paper') ||
+    (userChoice === 'paper' && computerChoice === 'rock')
+  ) {
+    result = 'You win!';
+  } else {
+    result = 'You lose!';
+  }
+  resultDisplay.innerHTML = result;
 }
